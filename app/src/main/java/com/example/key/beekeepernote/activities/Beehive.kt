ab14818ce264ae916ad64cd9a7eb8ace2c47a5f6
freevvm <freevvm@gmail.com>
2019-08-05 21:24:00 +0300

    new vvm test

---
+++ app/src/main/java/com/example/key/beekeepernote/activities/Beehive.kt
@@ -0,0 +1,14 @@
+package com.example.key.beekeepernote.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.key.beekeepernote.R
+
+class Beehive : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_beehive)
+        val buttonId = intent.getIntExtra("button_id",0)
+    }
+}
--- app/src/main/java/com/example/key/beekeepernote/activities/MainActivity.kt
+++ app/src/main/java/com/example/key/beekeepernote/activities/MainActivity.kt
@@ -1,15 +1,33 @@
 package com.example.key.beekeepernote.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 
 import com.example.key.beekeepernote.R
+import com.example.key.beekeepernote.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.name1Button.setOnClickListener { startNewActivities(it) }
+    }
+
+    private fun startNewActivities(view: View) {
+        val intent = Intent (this, Beehive::class.java)
+        intent.putExtra("button_id",view.id)
+        startActivity(intent)
+    }
+    fun toastMe(view: View) {
+        // val myToast = Toast.makeText(this, message, duration);
+        val myToast = Toast.makeText(this, "Hello!!!", Toast.LENGTH_SHORT)
+        myToast.show()
     }
 }
